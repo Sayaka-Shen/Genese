806db4dd7affaa1b80692ea3bf1e2c95a9fa8a3e
Decisy Gaëtan <gaetan.decisy6@hotmail.com>
2024-10-18 21:12:43 +0200

    Change collider and music

--- Assets/Scripts/InteractionObject/Bottes.cs
+++ Assets/Scripts/InteractionObject/Bottes.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bottes : MonoBehaviour
+public class Bottes : InteractableObject
 {
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/InteractionObject/Epee.cs
+++ Assets/Scripts/InteractionObject/Epee.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Epee : MonoBehaviour
+public class Epee : InteractableObject
 {
     // Start is called before the first frame update
     void Start()
